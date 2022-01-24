f782ca470a4f1463a9f3f1af898247cd982e9080
shivani <shivaninimmy@gmail.com>
2022-01-24 01:37:44 -0500

    SOLID principles
    SO- completely done and primary lazy annotations use cases included.

---
+++ Spring-boot-solid-example/src/main/java/com/solidprinciples/Spring_boot_solid_example/App.java
@@ -0,0 +1,16 @@
+package com.solidprinciples.Spring_boot_solid_example;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(primaryConfig.class);
+        
+    }
+}
